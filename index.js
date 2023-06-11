342d721968444ede1882d6e8dea30a32631f4281
Pere <gpere800@gmail.com>
2023-06-10 23:31:17 -0700

    modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ function getdata() {
 var user = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 var cpassword = document.getElementById('cpassword').value;
+var age = document.getElementById('age').value;
 
 if (password != cpassword){
     document.getElementById('password_msg').innerHTML = 'password mismatch'
-    document.getElementById('fields_msg').innerHTML = 'null'
+    document.getElementById('fields_msg').innerHTML = ''
 }else if (password == cpassword){
-    document.getElementById('password_msg').innerHTML = null
-    document.getElementById('fields_msg').innerHTML = 'null'
+    document.getElementById('password_msg').innerHTML = ''
+    document.getElementById('fields_msg').innerHTML = ''
 }
+if (age < 5){
+    document.getElementById('fields_msg').innerHTML = 'you are too young to fill this'
+}else if (age > 100){
+    document.getElementById('fields_msg').innerHTML = 'you are too old to fill this'
+}
+
+
 
 
 if (!user || !password || !cpassword ){
@@ -20,7 +28,7 @@ if (!user || !password || !cpassword ){
     dataStore.push(`username: ${user}`);
     dataStore.push(`password: ${password}`);
     dataStore.push(`confirmed password: ${cpassword}`);
-    document.getElementById('fields_msg').innerHTML = 'success';
+    // document.getElementById('fields_msg').innerHTML = 'success';
 
 
 
