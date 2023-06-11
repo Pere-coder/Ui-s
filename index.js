c2d4fa897306c341b42802ad4264e027fb1c3996
Pere <gpere800@gmail.com>
2023-06-11 08:20:13 -0700

    modified:   index.js

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ var cpassword = document.getElementById('cpassword').value;
 var age = document.getElementById('age').value;
 
 if (password != cpassword){
-    document.getElementById('password_msg').innerHTML = 'password mismatch'
+    document.getElementById('password_msg').innerHTML = 'password mismatch;'
     document.getElementById('fields_msg').innerHTML = ''
 }else if (password == cpassword){
     document.getElementById('password_msg').innerHTML = ''
     document.getElementById('fields_msg').innerHTML = ''
 }
 if (age < 5){
-    document.getElementById('fields_msg').innerHTML = 'you are too young to fill this'
+    document.getElementById('fields_msg').innerHTML = 'you are too young to fill this;'
 }else if (age > 100){
-    document.getElementById('fields_msg').innerHTML = 'you are too old to fill this'
+    document.getElementById('fields_msg').innerHTML = 'you are too old to fill this;'
 }
 
 
 
 
 if (!user || !password || !cpassword ){
-    document.getElementById('fields_msg').innerHTML = 'please fill all fields '
+    document.getElementById('fields_msg').innerHTML = 'please fill all fields;'
 }else if (user || password || cpassword){
     dataStore.push(`username: ${user}`);
     dataStore.push(`password: ${password}`);
